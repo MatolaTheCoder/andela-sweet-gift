831b2f3a58c1338a8d72f18b36d6ae3a0d9e4dd9
MatolaTheCoder <isacmatolajr6@gmail.com>
2024-03-19 22:38:41 +0300

    removing the app.css

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-import './App.css';
 
-export default function App() {
+function App() {
   return (
-    <h1 className="text-xl font-bold underline">
+    <div className="App">
+      <h1 className="text-3xl font-bold underline">
       Hello world!
-    </h1>
-  )
+      </h1>
+    </div>
+  );
 }
+
+export default App;
