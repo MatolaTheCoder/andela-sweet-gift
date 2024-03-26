c97934a60f3440861e88a03e48fecf91a4c6d847
MatolaTheCoder <isacmatolajr6@gmail.com>
2024-03-26 11:07:41 +0300

    nem sei

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 
+import React from 'react'
+import router from './main';
+
 function App() {
   return (
     <div className="App">
-      <h1 className="text-3xl font-bold underline">
-      Hello world!
-      </h1>
+      <router/>
     </div>
   );
 }
---
+++ src/main.jsx
@@ -0,0 +1,25 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import "./index.css";
+import Root from "./routes/root";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+  },
+  {
+    path: "/contacts",
+    element: <Root />,
+  },
+]);
+export default router
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
