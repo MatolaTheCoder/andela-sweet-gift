3db3860179b4206d4a57df0f7980afc9d58345ee
MatolaTheCoder <isacmatolajr6@gmail.com>
2024-04-11 22:52:41 +0200

    logo

--- src/components/shared/Sidebar.jsx
+++ src/components/shared/Sidebar.jsx
@@ -4,8 +4,8 @@ import { FcBarChart } from "react-icons/fc";
 function Sidebar() {
   return (
     <div>
-      <div className='bg-purple-200 w-[190px] p-3.5 h-screen flex flex-col'>
-            <div className='flex  items-center gap-2'><FcBarChart /><span>Andela</span></div>
+      <div className='bg-purple-200 w-[190px] p-4 h-screen flex flex-col'>
+            <div className='flex  items-center gap-2 py-3'><FcBarChart fontSize={24}/><span className='text-gray-700 text-lg'>Andela</span></div>
             <div className='flex-1'>Sidebar top</div>
             <div>Sidebar bottom</div>
         </div>
