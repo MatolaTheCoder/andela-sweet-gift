6428e475fc0e74cf35a38968c7e835ee64bdc802
MatolaTheCoder <isacmatolajr6@gmail.com>
2024-04-11 22:20:54 +0200

    react-icon

--- src/components/shared/Sidebar.jsx
+++ src/components/shared/Sidebar.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
+import { FcBarChart } from "react-icons/fc";
 
 function Sidebar() {
   return (
     <div>
       <div className='bg-purple-200 w-[190px] p-3.5 h-screen flex flex-col'>
-          <div>Sidebar</div>
-          <div>Sidebar</div>
+            <div><FcBarChart />Andela</div>
+            <div className='flex-1'>Sidebar top</div>
+            <div>Sidebar bottom</div>
         </div>
     </div>
   )
