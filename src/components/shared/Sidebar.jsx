291e36570c6de51b7ae43e061525726e51e97b65
MatolaTheCoder <isacmatolajr6@gmail.com>
2024-04-11 22:35:04 +0200

    Sidebar logo

--- src/components/shared/Sidebar.jsx
+++ src/components/shared/Sidebar.jsx
@@ -5,7 +5,7 @@ function Sidebar() {
   return (
     <div>
       <div className='bg-purple-200 w-[190px] p-3.5 h-screen flex flex-col'>
-            <div><FcBarChart />Andela</div>
+            <div className='flex  items-center gap-2'><FcBarChart /><span>Andela</span></div>
             <div className='flex-1'>Sidebar top</div>
             <div>Sidebar bottom</div>
         </div>
