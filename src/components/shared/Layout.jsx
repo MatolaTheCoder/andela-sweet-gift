a9b206a155ee0cd03a6dd3409e9205918d8e787e
MatolaTheCoder <isacmatolajr6@gmail.com>
2024-04-11 22:04:45 +0200

    sidebar

--- src/components/shared/Layout.jsx
+++ src/components/shared/Layout.jsx
@@ -1,13 +1,12 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
+import Sidebar from './Sidebar';
 
 const Layout = ({ children }) => {
   return (
     <>
       <div className='flex flex-row bg-neutral-100 h-screen w-screen overflow-hidden'>
-        <div className='bg-purple-200 w-[190px]'>
-          <h1>Sidebar</h1>
-        </div>
+        <Sidebar/>
         <div className='p-4'>
           <div className='bg-violet-400'>
             <p>Header</p>
---
+++ src/components/shared/Sidebar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Sidebar() {
+  return (
+    <div>
+      <div className='bg-purple-200 w-[190px] p-3.5 h-screen flex flex-col'>
+          <div>Sidebar</div>
+          <div>Sidebar</div>
+        </div>
+    </div>
+  )
+}
+
+export default Sidebar
