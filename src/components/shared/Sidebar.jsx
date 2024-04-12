4caf663068614d2eb2c52f1d99b5df3352279a3f
MatolaTheCoder <isacmatolajr6@gmail.com>
2024-04-12 19:11:25 +0200

    sidebar link

--- src/components/shared/Sidebar.jsx
+++ src/components/shared/Sidebar.jsx
@@ -7,7 +7,9 @@ export default function Sidebar() {
   return (
     <div>
       <div className='bg-purple-900 w-[190px] p-4 h-screen flex flex-col'>
-            <div className='flex  items-center gap-2 py-3 border-b-2 border-gray-500'><FcBarChart fontSize={24}/><span className='text-gray-700 text-lg'>Andela</span></div>
+            <div className='flex  items-center gap-2 py-3 border-b-2 border-gray-500'><FcBarChart fontSize={24}/>
+                <span className='text-gray-400 text-lg font-bold'>Andela</span>
+            </div>
             <div className='flex-1'>{DASHBOARD_SIDEBAR_LINKS.map((item) => (
                 <SideBarLinks key={item.key} item={item}/>
                 ))}
@@ -21,7 +23,7 @@ export default function Sidebar() {
 function SideBarLinks({item}){
     return(
         <Link to={item.path}>
-            <div className='flex items-center gap-2 py-2 px-3 font-light hover:text-purple-500 active:text-purple-400 text-base'>
+            <div className='flex text-slate-100 items-center gap-2 py-2 px-3 font-light hover:bg-purple-800 active:text-purple-500 active:bg-purple-800 text-base rounded-lg'>
                 <span className='text-xl '>{item.icon}</span>
                 {item.label}
             </div>
