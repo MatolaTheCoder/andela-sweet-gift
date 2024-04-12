25dff55a94f6d0555b1ba61f82acb4461492a733
MatolaTheCoder <isacmatolajr6@gmail.com>
2024-04-12 19:35:29 +0200

    Links

--- src/lib/constants/index.jsx
+++ src/lib/constants/index.jsx
@@ -18,7 +18,7 @@ export const DASHBOARD_SIDEBAR_LINKS = [
     {
         key: 'products',
         label: 'Products',
-        path: '/products',
+        path: '/produtos',
         icon: <HiOutlineCube/>
     },
     {
