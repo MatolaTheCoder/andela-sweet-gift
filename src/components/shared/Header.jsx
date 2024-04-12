35644a18ebb49bd3d21ae2897b4d7985e735abc8
MatolaTheCoder <isacmatolajr6@gmail.com>
2024-04-12 23:04:06 +0200

    Header implementation

--- src/components/shared/Header.jsx
+++ src/components/shared/Header.jsx
@@ -2,8 +2,9 @@ import React from 'react'
 
 export default function Header() {
   return (
-    <div>
-        Header
+    <div className='bg-white h-16 px-4 py-1 flex justify-between items-center'>
+        <div className=''>Search</div>
+        <div >But</div>
     </div>
   )
 }
