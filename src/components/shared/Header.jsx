ca6f545eb87ea4bcf0769a9a168432c5417c58d5
MatolaTheCoder <isacmatolajr6@gmail.com>
2024-04-12 23:28:57 +0200

    shadow property

--- src/components/shared/Header.jsx
+++ src/components/shared/Header.jsx
@@ -3,7 +3,7 @@ import { HiOutlineSearch } from 'react-icons/hi'
 
 export default function Header() {
   return (
-    <div className='bg-white h-16 px-4 py-1 flex justify-between items-center '>
+    <div className='bg-white h-16 px-4 py-1 flex justify-between items-center shadow'>
         <div className='relative'>
             <HiOutlineSearch className='text-gray-500 absolute top-1/2 -translate-y-1/2 left-2'/>
             <input type="text" placeholder='Pesquise...' className='border border-gray-300 rounded-md h-8 w-[24rem] px-8 focus:outline-none text-sm'/>
