2ba1e6860de9c5159f488feb5f5642470a0d282e
MatolaTheCoder <isacmatolajr6@gmail.com>
2024-04-13 01:36:00 +0200

    Header popups

--- src/components/shared/Header.jsx
+++ src/components/shared/Header.jsx
@@ -10,12 +10,13 @@ export default function Header() {
             <input type="text" placeholder='Pesquise...' className='border border-gray-300 rounded-md h-8 w-[24rem] px-8 focus:outline-none text-sm'/>
         </div>
         <div className='flex gap-3.5 mr-1.5'>
-            
-        < Popover className="relative">
-                <Popover.Button><HiOutlineChatAlt2 fontSize={24}/></Popover.Button>
-                <Popover.Panel className=" absolute end-8 z-10 w-80 ">
+        {/* Pop up Messages*/}    
+            <Popover className="relative">
+                <Popover.Button><HiOutlineChatAlt2 fontSize={24} className='focus:outline-none focus:bg-gray-200 active:bg-gray-200'/></Popover.Button>
+                <Popover.Overlay className="fixed inset-0 bg-black opacity-30 " />
+                <Popover.Panel className=" absolute end-5 z-10 w-80 ">
                     <div className="grid grid-cols-1 bg-white rounded px-5 py-3">
-                        <strong className='text-gray-700'>Messagem</strong>
+                        <strong className='text-red-400'>Messagem</strong>
                         <div className='mt-2 text-sm text-gray-500 '>
                             Nenhuma mensagem
                         </div>
@@ -32,24 +33,32 @@ export default function Header() {
             </Popover>
 
            < Popover className="relative">
-                <Popover.Button><HiOutlineBell fontSize={24}/></Popover.Button>
-                <Popover.Panel className=" absolute end-6 z-10 w-80">
-                <div className="grid grid-cols-1 bg-white rounded px-5 py-3">
-                        <strong className='text-gray-700'>Notificacao</strong>
-                        <div className='mt-2 text-sm text-gray-500 '>
-                            Nenhuma notificacao
-                        </div>
-                    </div>
-                </Popover.Panel>
-                <Transition
-                    enter="transition duration-100 ease-out"
-                    enterFrom="transform scale-95 opacity-0"
-                    enterTo="transform scale-100 opacity-100"
-                    leave="transition duration-75 ease-out"
-                    leaveFrom="transform scale-100 opacity-100"
-                    leaveTo="transform scale-95 opacity-0"
-                ></Transition>
+                {({open}) => (
+                    <>
+                        <Popover.Button><HiOutlineBell fontSize={24} className='focus:outline-none focus:bg-gray-200 active:bg-gray-200'/></Popover.Button>
+                        <Popover.Overlay className="fixed inset-0 bg-black opacity-30 " />
+                        <Popover.Panel className=" absolute end-6 z-10 w-80">
+                        <div className="grid grid-cols-1 bg-white rounded px-5 py-3">
+                                <strong className='text-red-400'>Notificacao</strong>
+                                <div className='mt-2 text-sm text-gray-500 '>
+                                    Nenhuma notificacao disponivel
+                                </div>
+                            </div>
+                        </Popover.Panel>
+                        <Transition
+                            enter="transition duration-100 ease-out"
+                            enterFrom="transform scale-95 opacity-0"
+                            enterTo="transform scale-100 opacity-100"
+                            leave="transition duration-75 ease-out"
+                            leaveFrom="transform scale-100 opacity-100"
+                            leaveTo="transform scale-95 opacity-0"
+                        ></Transition>
+                    </>
+                
+                )}
             </Popover>
+
+{/**======================================User Options============================================*/}
         </div>
     </div>
   )
