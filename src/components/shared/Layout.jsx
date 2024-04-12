b35ca833045385ae5213116e939ce3e4e675c146
MatolaTheCoder <isacmatolajr6@gmail.com>
2024-04-12 23:04:20 +0200

    Layout idea

--- src/components/shared/Layout.jsx
+++ src/components/shared/Layout.jsx
@@ -6,16 +6,13 @@ import Header from './Header';
 const Layout = ({ children }) => {
   return (
     <>
-      <div className='flex flex-row bg-neutral-100 h-screen w-screen overflow-hidden'>
+      <div className='flex flex-row bg-neutral-200 h-screen w-screen overflow-hidden'>
         <Sidebar/>
-        <div className='p-4'>
+        <div className='flex-1'>
           <div >
             <Header/>
           </div>
-          <div><Outlet/></div>
-          <div className='bg-gray-400'>
-            Footer
-          </div>
+          <div className='p-4'><Outlet/></div>
         </div>
       </div> 
     </>
