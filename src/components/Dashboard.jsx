9654488932a5c9dc862b4cf3bb9344252ad44dfe
MatolaTheCoder <isacmatolajr6@gmail.com>
2024-04-14 20:41:05 +0200

    importing the stats grid

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import DashboardStatsGrid from './DashboardStatsGrid'
 
 export default function Dashboard() {
   return (
     <div>
-      <h1>Dashboard</h1>
+      <DashboardStatsGrid/>
     </div>
   )
 }
