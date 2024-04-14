c7727992a304557cfa462660f4daa4cbc5d273f4
MatolaTheCoder <isacmatolajr6@gmail.com>
2024-04-14 20:49:57 +0200

    Styling the stats grid

--- src/components/DashboardStatsGrid.jsx
+++ src/components/DashboardStatsGrid.jsx
@@ -4,10 +4,10 @@ export default function DashboardStatsGrid() {
   return (
     <>
       <div className='flex gap-2'>
-        <div className='bg-white'>1234</div>
-        <div className='bg-white'>1234</div>
-        <div className='bg-white'>1234</div>
-        <div className='bg-white'>1234</div>
+        <div className='bg-white p-4 flex-1 rounded shadow flex items-center'>1234</div>
+        <div className='bg-white p-4 flex-1 rounded shadow flex items-center'>1234</div>
+        <div className='bg-white p-4 flex-1 rounded shadow flex items-center'>1234</div>
+        <div className='bg-white p-4 flex-1 rounded shadow flex items-center'>1234</div>
       </div>
     </>
   )
