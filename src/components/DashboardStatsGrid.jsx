ef395f9f6589e8745ffba849e4be081b63a34e4f
MatolaTheCoder <isacmatolajr6@gmail.com>
2024-04-14 23:20:00 +0200

    organizing the code

--- src/components/DashboardStatsGrid.jsx
+++ src/components/DashboardStatsGrid.jsx
@@ -4,11 +4,15 @@ export default function DashboardStatsGrid() {
   return (
     <>
       <div className='flex gap-2'>
-        <div className='bg-white p-4 flex-1 rounded shadow flex items-center'>1234</div>
-        <div className='bg-white p-4 flex-1 rounded shadow flex items-center'>1234</div>
-        <div className='bg-white p-4 flex-1 rounded shadow flex items-center'>1234</div>
-        <div className='bg-white p-4 flex-1 rounded shadow flex items-center'>1234</div>
+        <GridWrapper>123</GridWrapper>
+        <GridWrapper>123</GridWrapper>
+        <GridWrapper>123</GridWrapper>
+        <GridWrapper>123</GridWrapper>
       </div>
     </>
   )
+}
+
+function GridWrapper({children}){
+  return <div className='bg-white p-4 flex-1 rounded shadow flex items-center'>{children}</div>
 }
