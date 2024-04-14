bab8a62935f160eaecb1cd7f9a874004537b1511
MatolaTheCoder <isacmatolajr6@gmail.com>
2024-04-14 20:37:30 +0200

    styling the grid

--- src/components/DashboardStatsGrid.jsx
+++ src/components/DashboardStatsGrid.jsx
@@ -3,7 +3,12 @@ import React from 'react'
 export default function StatsGrid() {
   return (
     <>
-      
+      <div className='flex gap-2'>
+        <div className='bg-white'>1234</div>
+        <div className='bg-white'>1234</div>
+        <div className='bg-white'>1234</div>
+        <div className='bg-white'>1234</div>
+      </div>
     </>
   )
 }
