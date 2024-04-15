75407f372eaf1c2191d5ac657502d2edda5467c1
MatolaTheCoder <isacmatolajr6@gmail.com>
2024-04-15 11:19:27 +0200

    grid styling

--- src/components/DashboardStatsGrid.jsx
+++ src/components/DashboardStatsGrid.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
+import { HiOutlineShoppingCart } from 'react-icons/hi'
 
 export default function DashboardStatsGrid() {
   return (
     <>
       <div className='flex gap-2'>
-        <GridWrapper>123</GridWrapper>
+        <GridWrapper>
+          <div className='bg-cyan-500 text-xl w-10 h-10 flex items-center justify-center rounded-full text-white m-2'>
+            <HiOutlineShoppingCart/>
+          </div>
+          <div className='flex flex-col pl-3'>
+            <p className='font-bold text-sm text-gray-500'>Numero de encomendas</p>
+            <p className='font-bold text-xl text-gray-600'>20</p>            
+          </div>
+        </GridWrapper>
         <GridWrapper>123</GridWrapper>
         <GridWrapper>123</GridWrapper>
         <GridWrapper>123</GridWrapper>
