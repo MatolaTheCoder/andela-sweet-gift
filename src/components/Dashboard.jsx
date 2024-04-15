3406838a96f9a23b290650d3e56492d47eeee280
MatolaTheCoder <isacmatolajr6@gmail.com>
2024-04-15 11:35:36 +0200

    Dashboard Chart

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import DashboardStatsGrid from './DashboardStatsGrid'
+import DashboardChart from './DashboardChart'
 
 export default function Dashboard() {
   return (
     <div>
       <DashboardStatsGrid/>
+      <DashboardChart/>
     </div>
   )
 }
