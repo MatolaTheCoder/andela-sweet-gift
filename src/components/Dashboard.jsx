4120c85497defa0b8f87671c0aae63368bae37ef
MatolaTheCoder <isacmatolajr6@gmail.com>
2024-04-21 00:34:10 +0200

    Orders table

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import DashboardStatsGrid from './DashboardStatsGrid'
 import DashboardChart from './DashboardChart'
 import DashboardBuyerChart from './DashboardBuyerChart'
+import RecentOrders from './RecentOrders'
 
 export default function Dashboard() {
   return (
@@ -12,7 +13,7 @@ export default function Dashboard() {
         <DashboardBuyerChart/>
       </div>
       <div className="flex flex-row gap-4 w-full">
-        
+        <RecentOrders/>
       </div>
     </div>
   )
