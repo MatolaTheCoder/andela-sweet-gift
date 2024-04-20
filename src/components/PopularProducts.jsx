357b1d68575bc27363b1dafb2fbe5eae20b260ba
MatolaTheCoder <isacmatolajr6@gmail.com>
2024-04-21 00:39:25 +0200

    Popular products

---
+++ src/components/PopularProducts.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function PopularProducts() {
+  return (
+    <>
+      <div className="bg-white p-4 w-full h-full flex flex-col flex-1">
+        <div className="font-md text-gray-600"><p>Popular Products</p></div>
+        
+      </div>
+    </>
+  )
+}
+
+export default PopularProducts
