a3d69b3fdf01ba2b36386939098b1a6282de26b9
MatolaTheCoder <isacmatolajr6@gmail.com>
2024-04-21 00:36:57 +0200

    Recent orders text

--- src/components/RecentOrders.jsx
+++ src/components/RecentOrders.jsx
@@ -3,7 +3,8 @@ import React from 'react'
 function RecentOrders() {
   return (
     <>
-      <div className="bg-white p-4 w-full h-full">
+      <div className="bg-white p-4 w-full h-full flex flex-col flex-1">
+        <div className="font-md text-gray-600"><p>Recent Orders</p></div>
         
       </div>
     </>
