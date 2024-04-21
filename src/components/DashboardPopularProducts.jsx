de3dd06a3d4b90291745e4eef3beda8297496c7a
MatolaTheCoder <isacmatolajr6@gmail.com>
2024-04-21 21:23:06 +0200

    resizing the div

--- src/components/DashboardPopularProducts.jsx
+++ src/components/DashboardPopularProducts.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function PopularProducts() {
   return (
     <>
-      <div className="bg-white p-4 w-full h-full flex flex-col flex-1">
+      <div className="bg-white p-4 w-[20rem] h-full flex flex-col">
         <div className="font-md text-gray-600"><p>Popular Products</p></div>
         
       </div>
