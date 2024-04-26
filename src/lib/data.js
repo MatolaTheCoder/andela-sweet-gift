f658174a83974fc13852dbba6a408ec6408a8c49
MatolaTheCoder <isacmatolajr6@gmail.com>
2024-04-26 22:38:35 +0200

    modifying the price

--- src/lib/data.js
+++ src/lib/data.js
@@ -2,25 +2,25 @@ export const data_popular_product = [
     {
       id: 'Prod06',
       prod_name: 'Chavena Personalizada',
-      prod_price: 560,
+      prod_price: "560 MT",
       prod_stock: 3
     },
     {
       id: 'Prod12',
       prod_name: 'Quadro',
-      prod_price: 799,
+      prod_price: "799 MT",
       prod_stock: 5
     },
     {
       id: 'Prod18',
       prod_name: 'Buque',
-      prod_price: 1299,
+      prod_price: "1.299 MT",
       prod_stock: 8
     },
     {
       id: 'Prod24',
       prod_name: 'balao personalizado',
-      prod_price: 899,
+      prod_price: "899 MT",
       prod_stock: 10
     },
     // {
