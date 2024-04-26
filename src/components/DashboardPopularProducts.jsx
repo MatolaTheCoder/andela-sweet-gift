ab7cf433f6332b9a3b7daff28a2b88ca7f193ab6
MatolaTheCoder <isacmatolajr6@gmail.com>
2024-04-26 21:45:29 +0200

    styling

--- src/components/DashboardPopularProducts.jsx
+++ src/components/DashboardPopularProducts.jsx
@@ -6,10 +6,12 @@ function PopularProducts() {
     <>
       <div className="bg-white p-4 w-[20rem] h-full flex flex-col">
         <div className="font-md text-gray-600"><p>Popular Products</p></div>
-        <div className="mt-3">
+        <div className="mt-3 flex flex-col gap-3">
           {data_popular_product.map((order)=>(
-            <div key={order.id}>
-              
+            <div className='flex' key={order.id}>
+              <div className="w-10 p-10 rounded-sm bg-gray-200">
+                <img className='w-full h-full ' src="" alt={order.prod_name} />
+              </div>
             </div>
           ))}
         </div>
