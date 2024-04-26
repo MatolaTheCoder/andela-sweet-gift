10cc76fad2158110c9818889a9f4d54adffb2145
MatolaTheCoder <isacmatolajr6@gmail.com>
2024-04-26 21:25:32 +0200

    mapping the product orders data

--- src/components/DashboardPopularProducts.jsx
+++ src/components/DashboardPopularProducts.jsx
@@ -7,7 +7,11 @@ function PopularProducts() {
       <div className="bg-white p-4 w-[20rem] h-full flex flex-col">
         <div className="font-md text-gray-600"><p>Popular Products</p></div>
         <div className="mt-3">
-          
+          {data_popular_product.map((order)=>(
+            <div key={order.id}>
+              
+            </div>
+          ))}
         </div>
       </div>
     </>
