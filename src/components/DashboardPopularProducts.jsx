3b34a7e82bcac473752edb69f420758f7b1b9605
MatolaTheCoder <isacmatolajr6@gmail.com>
2024-04-26 21:19:52 +0200

    adding a data file that contain the popular orders data

--- src/components/DashboardPopularProducts.jsx
+++ src/components/DashboardPopularProducts.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
+import { data_popular_product } from '../lib/data'
 
 function PopularProducts() {
   return (
     <>
       <div className="bg-white p-4 w-[20rem] h-full flex flex-col">
         <div className="font-md text-gray-600"><p>Popular Products</p></div>
-        
+        <div className="mt-3">
+          
+        </div>
       </div>
     </>
   )
