babcc9d49a67676ddc92084a499d8d3e44825fd3
MatolaTheCoder <isacmatolajr6@gmail.com>
2024-04-27 00:44:47 +0200

    styling the produtos page

--- src/components/Produtos.jsx
+++ src/components/Produtos.jsx
@@ -1,11 +1,46 @@
 // Produtos.js
 import React from 'react';
-
+import { Link } from 'react-router-dom';
+import { data_produts } from '../lib/data';
+import { MdModeEdit,MdDelete } from 'react-icons/md';
 const Produtos = () => {
   return (
     <div>
-      <h2>Produtos</h2>
-      <p>Listagem de produtos...</p>
+      <div className='bg-white flex flex-col'>
+        <div className="title text-gray-700 flex text-lg p-4 font-semibold">
+          <p className='basis-1/2 mr-24'>Produtos</p>
+          <button className='text-white bg-sky-500 rounded-md p-2 text-sm'>Adicionar</button>
+        </div>
+        <div className="p-4 flex-1">
+        <table className='w-fit border-collapse border border-gray-200'>
+          <thead>
+            <tr className='bg-gray-100'>
+              <th className='p-4'>Codigo</th>
+              <th className='p-4'>Nome do produto</th>
+              <th className='p-4'>Categoria</th>
+              <th className='p-4'>Data de criação</th>
+              <th className='p-4'>Preço do produto</th>
+              <th className='p-4'>Operação</th>
+            </tr>
+          </thead>
+          <tbody>
+            {data_produts.map((data)=>(
+              <tr className='bg-white text-center'>
+                <td className='p-4'>{data.id}</td>
+                <td className='p-4'>{data.prod_name}</td>
+                <td className='p-4'>{data.prod_categoria}</td>
+                <td className='p-4'>{data.prod_Date}</td>
+                <td className='p-4'>{data.prod_price}</td>
+                <td className='p-4 ml-5 text-xl flex gap-2'><Link className='hover:text-2xl text-sky-500'><MdModeEdit/></Link><Link className='hover:text-2xl text-red-500'><MdDelete/></Link></td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+
+        </div>
+      </div>
+      
+      
     </div>
   );
 }
