0035baffbfbb579adeddb26d50761b0ff5ee4736
MatolaTheCoder <isacmatolajr6@gmail.com>
2024-04-27 00:21:04 +0200

    adding a data and a category to the products data

--- src/lib/data.js
+++ src/lib/data.js
@@ -30,30 +30,35 @@ export const data_popular_product = [
       id: 'Prod06',
       prod_name: 'Chavena Personalizada',
       prod_price: "560 MT",
-      prod_stock: 3
+      prod_Date: '2024-04-20:15:45',
+      prod_categoria: 3
     },
     {
       id: 'Prod12',
       prod_name: 'Quadro',
       prod_price: "799 MT",
-      prod_stock: 5
+      prod_Date: '2024-04-20:15:45',
+      prod_categoria: 5
     },
     {
       id: 'Prod18',
       prod_name: 'Buque',
       prod_price: "1.299 MT",
-      prod_stock: 8
+      prod_Date: '2024-04-20:15:45',
+      prod_categoria: 8
     },
     {
       id: 'Prod24',
       prod_name: 'balao personalizado',
       prod_price: "899 MT",
-      prod_stock: 10
+      prod_Date: '2024-04-20:15:45',
+      prod_categoria: 10
     },
      {
        id: 'Prod30',
        prod_name: 'Toalha personalizada',
        prod_price: 499,
-       prod_stock: 6
+       prod_categoria: 6,
+       prod_Date: '2024-04-20:15:45',
      }
   ]
