df7424da95360d2630f9c7e2ea6eaadeb1e67b07
MatolaTheCoder <isacmatolajr6@gmail.com>
2024-05-24 23:05:49 +0200

    style: removed unnecessary style from title and table divs

--- src/components/Produtos.jsx
+++ src/components/Produtos.jsx
@@ -6,37 +6,36 @@ import { MdModeEdit,MdDelete } from 'react-icons/md';
 const Produtos = () => {
   return (
     <div>
-      <div className='bg-white flex flex-col'>
-        <div className="title text-gray-700 flex w-[100px] text-lg p-4 font-semibold">
-          <p className='basis-1/2 mr-24'>Produtos</p>
-          <button className='text-white bg-sky-500 rounded-md p-2 text-sm flex flex-1'><Link to="/produtos/add">Adicionar</Link></button>
+      <div className='bg-white flex flex-col items-center'>
+        <div className="title text-gray-700 flex w-1/2 text-lg p-4 font-semibold">
+          <p className=''>Produtos</p>
         </div>
-        <div className="p-4 flex-1">
-        <table className='w-fit border-collapse border border-gray-200'>
-          <thead>
-            <tr className='bg-gray-100'>
-              <th className='p-4'>Codigo</th>
-              <th className='p-4'>Nome do produto</th>
-              <th className='p-4'>Categoria</th>
-              <th className='p-4'>Data de criação</th>
-              <th className='p-4'>Preço do produto</th>
-              <th className='p-4'>Operação</th>
-            </tr>
-          </thead>
-          <tbody>
-            {data_produts.map((data)=>(
-              <tr className='bg-white text-center'>
-                <td className='p-4'>{data.id}</td>
-                <td className='p-4'>{data.prod_name}</td>
-                <td className='p-4'>{data.prod_categoria}</td>
-                <td className='p-4'>{data.prod_Date}</td>
-                <td className='p-4'>{data.prod_price}</td>
-                <td className='p-4 ml-5 text-xl flex gap-1'><Link className='hover:text-2xl text-sky-500'><MdModeEdit/></Link><Link className='hover:text-2xl text-red-500'><MdDelete/></Link></td>
+        <div className="p-4 ">
+          <table className='w-fit border-collapse border border-gray-200'>
+            <thead>
+              <tr className='bg-gray-100'>
+                <th className='p-4'>Codigo</th>
+                <th className='p-4'>Nome do produto</th>
+                <th className='p-4'>Categoria</th>
+                <th className='p-4'>Data de criação</th>
+                <th className='p-4'>Preço do produto</th>
+                <th className='p-4'>Operação</th>
               </tr>
-            ))}
-          </tbody>
-        </table>
-
+            </thead>
+            <tbody>
+              {data_produts.map((data)=>(
+                <tr className='bg-white text-center'>
+                  <td className='p-4'>{data.id}</td>
+                  <td className='p-4'>{data.prod_name}</td>
+                  <td className='p-4'>{data.prod_categoria}</td>
+                  <td className='p-4'>{data.prod_Date}</td>
+                  <td className='p-4'>{data.prod_price}</td>
+                  <td className='p-4 ml-5 text-xl flex gap-1'><Link className='hover:text-2xl text-sky-500'><MdModeEdit/></Link><Link className='hover:text-2xl text-red-500'><MdDelete/></Link></td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        <button className='text-white bg-sky-500 rounded-md p-2 text-sm flex'><Link to="/produtos/add">Adicionar</Link></button>
         </div>
       </div>
       
