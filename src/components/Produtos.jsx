a8b01d1f217b7084119b00e7061af5a4b5d13d5a
MatolaTheCoder <isacmatolajr6@gmail.com>
2025-01-04 01:19:51 +0200

    feat: refactor layout and header components

--- src/components/Produtos.jsx
+++ src/components/Produtos.jsx
@@ -1,6 +1,6 @@
 import { Link } from 'react-router-dom';
 import { data_produts } from '../lib/data';
-import { MdModeEdit,MdDelete } from 'react-icons/md';
+import { MdModeEdit, MdDelete } from 'react-icons/md';
 import React, { useState } from 'react';
 import { Dialog } from '@headlessui/react';
 const Produtos = () => {
@@ -8,71 +8,116 @@ const Produtos = () => {
 
   return (
     <div>
-      <div className='bg-white flex flex-col items-center min-h-screen'>
-        <div className="title text-gray-700 flex justify-center w-full text-lg p-4 font-semibold ">
+      <div className=' flex flex-col min-h-screen w-full'>
+        <div className="title text-gray-700 flex text-lg p-4 font-semibold ">
           <p className=''>Produtos</p>
         </div>
-        <div className="p-4 ">
-          <table className='w-fit border-collapse border border-gray-200'>
-            <thead className='bg-gray-600 text-white'>
-              <tr className=''>
-                <th className='text-left py-3 px-4 uppercase font-semibold text-sm'>Codigo</th>
-                <th className='text-left py-3 px-4 uppercase font-semibold text-sm'>Nome do produto</th>
-                <th className='text-left py-3 px-4 uppercase font-semibold text-sm'>Categoria</th>
-                <th className='text-left py-3 px-4 uppercase font-semibold text-sm'>Data de criação</th>
-                <th className='text-left py-3 px-4 uppercase font-semibold text-sm'>Preço do produto</th>
-                <th className='text-left py-3 px-4 uppercase font-semibold text-sm'>Operação</th>
-              </tr>
-            </thead>
-            <tbody>
-              {data_produts.map((data)=>(
-                <tr className='bg-white text-center'>
-                  <td className='p-4'>{data.id}</td>
-                  <td className='p-4'>{data.prod_name}</td>
-                  <td className='p-4'>{data.prod_categoria}</td>
-                  <td className='p-4'>{data.prod_Date}</td>
-                  <td className='p-4'>{data.prod_price}</td>
-                  <td className='p-4 ml-5 text-xl flex gap-1'>
-                    <Link className='hover:text-2xl text-sky-500'><MdModeEdit/></Link>
-                    <button className='hover:text-2xl text-red-500' onClick={()=>setIsOpen(true)}><MdDelete/></button>
-                    <Dialog open={isOpen} onClose={() => setIsOpen(false)} className="relative z-50">
-                      <div className="fixed inset-0 bg-black bg-opacity-30"></div>  {/* Background overlay */}
-                      <div className="fixed inset-0 flex items-center justify-center p-4">
-                        <Dialog.Panel className="max-w-lg w-full space-y-4 border bg-white p-12">
-                          <Dialog.Title className="font-bold text-lg">Eliminar produto</Dialog.Title>
-                          <Dialog.Description className="text-gray-500">
-                            Isso nao pode ser revertido
-                          </Dialog.Description>
-                          <p>Voce tem a certeza de que deseja eliminar esse produto?</p>
-                          <div className="flex gap-4">
-                            <button 
-                              onClick={() => setIsOpen(false)} 
-                              className="bg-gray-300 hover:bg-gray-400 text-black font-bold py-2 px-4 rounded"
-                            >
-                              Cancel
-                            </button>
-                            <button 
-                              onClick={() => setIsOpen(false)} 
-                              className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
-                            >
-                              Eliminar
-                            </button>
+        <div className="w-full bg-white">
+          <p className="p-4 text-gray-700 font-semibold text-lg mb-4">| Produtos</p>
+          <div className="w-full max-h-[400px] overflow-y-auto">
+            <div className="p-6 bg-white max-w-5xl rounded-lg ">
+              <table className="w-full border-collapse bg-white shadow-sm rounded-lg ">
+                <thead>
+                  <tr className="bg-gray-200 text-gray-700 border-b">
+                    <th className="text-[12px] uppercase tracking-wide font-medium py-3 px-4 text-left">Quarto</th>
+                    <th className="text-[12px] uppercase tracking-wide font-medium py-3 px-4 text-left">Nome do produto</th>
+                    <th className="text-[12px] uppercase tracking-wide font-medium py-3 px-4 text-left">Categoria</th>
+                    <th className="text-[12px] uppercase tracking-wide font-medium py-3 px-4 text-left">Data de criacao</th>
+                    <th className="text-[12px] uppercase tracking-wide font-medium py-3 px-4 text-left">Preço</th>
+                    <th className="text-[12px] uppercase tracking-wide font-medium py-3 px-4 text-left">Opções</th>
+                  </tr>
+                </thead>
+                <tbody className="text-gray-700">
+                  {data_produts.map((data, index) => (
+                    <tr
+                      key={data.id}
+                      className={`${index % 2 === 0 ? "bg-gray-50" : "bg-white"
+                        } hover:bg-gray-100 transition-colors`}
+                    >
+                      <td className="py-3 px-4 border-b">
+                        <a
+                          href="#"
+                          className="text-gray-600 text-sm font-medium hover:text-blue-500 truncate"
+                        >
+                          {data.id}
+                        </a>
+                      </td>
+                      <td className="py-3 px-4 border-b">
+                        <span className="text-[13px] font-medium text-gray-500">
+                          {data.prod_name}
+                        </span>
+                      </td>
+                      <td className="py-3 px-4 border-b">
+                        <span className="text-[13px] font-medium text-gray-500">
+                          {data.prod_categoria}
+                        </span>
+                      </td>
+                      <td className="py-3 px-4 border-b">
+                        <span className="text-[13px] font-medium text-gray-500">
+                          {data.prod_Date}
+                        </span>
+                      </td>
+                      <td className="py-3 px-4 border-b">
+                        <span className="text-[13px] font-medium text-gray-500">
+                          {data.prod_price}
+                        </span>
+                      </td>
+                      <td className="py-3 px-4 border-b flex items-center gap-2">
+                        <Link
+                          to="#"
+                          className="text-sky-500 hover:text-sky-700 transition-transform transform hover:scale-110"
+                        >
+                          <MdModeEdit />
+                        </Link>
+                        <button
+                          className="text-red-500 hover:text-red-700 transition-transform transform hover:scale-110"
+                          onClick={() => setIsOpen(true)}
+                        >
+                          <MdDelete />
+                        </button>
+                        <Dialog
+                          open={isOpen}
+                          onClose={() => setIsOpen(false)}
+                          className="relative z-50"
+                        >
+                          <div className="fixed inset-0 bg-black bg-opacity-30"></div>
+                          <div className="fixed inset-0 flex items-center justify-center p-4">
+                            <Dialog.Panel className="max-w-lg w-full bg-white p-6 shadow-lg rounded-md">
+                              <Dialog.Title className="font-bold text-lg text-gray-800">
+                                Eliminar Produto
+                              </Dialog.Title>
+                              <Dialog.Description className="text-gray-500 mb-4">
+                                Esta ação não pode ser revertida.
+                              </Dialog.Description>
+                              <p className="text-sm text-gray-600 mb-6">
+                                Tem certeza de que deseja eliminar este produto?
+                              </p>
+                              <div className="flex justify-end gap-4">
+                                <button
+                                  onClick={() => setIsOpen(false)}
+                                  className="bg-gray-300 hover:bg-gray-400 text-black font-bold py-2 px-4 rounded"
+                                >
+                                  Cancelar
+                                </button>
+                                <button
+                                  onClick={() => setIsOpen(false)}
+                                  className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
+                                >
+                                  Eliminar
+                                </button>
+                              </div>
+                            </Dialog.Panel>
                           </div>
-                        </Dialog.Panel>
-                      </div>
-                    </Dialog>
-                  </td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-        </div>
-        <div className="mt-10">
-          <button className='text-white bg-sky-500 rounded-md p-2 text-sm flex'><Link to="/produtos/add">Adicionar</Link></button>
+                        </Dialog>
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </div>
+          </div>
         </div>
       </div>
-      
-      
     </div>
   );
 }
