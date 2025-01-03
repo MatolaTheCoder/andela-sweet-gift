9bed71cd33a72382727d3740da40c00b64a2c742
MatolaTheCoder <isacmatolajr6@gmail.com>
2025-01-04 01:11:33 +0200

    feat: caminhos para os componentes criados

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,10 @@ import Dashboard from './components/Dashboard';
 import Produtos from './components/Produtos';
 import Cliente from './components/Cliente';
 import ProdutosAdd from './components/ProdutosAdd';
+import Perfil from './components/auth/Perfil';
+import EncomendaAdd from './components/EncomendaAdd';
+import Encomenda from './components/Encomenda';
+import Pagamentos from './components/Pagamentos';
 
 function App() {
   return (
@@ -12,9 +16,13 @@ function App() {
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Dashboard />} />
+          <Route path="perfil" element={<Perfil />} />
           <Route path="produtos" element={<Produtos />} />
           <Route path="produtos/add" element={<ProdutosAdd/>}/>
+          <Route path="encomendas/list" element={<Encomenda/>}/>
+          <Route path="encomendas/add" element={<EncomendaAdd/>}/>
           <Route path='/cliente/:id' element={<Cliente/>}/>
+          <Route path='/pagamento' element={<Pagamentos/>}/>
         </Route>
         <Route path='login' element={<>This is a login page</>}/>
       </Routes>
