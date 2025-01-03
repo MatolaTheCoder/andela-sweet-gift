3aa398c21a126dcd49b32aadc80b5f9f6869dc4d
MatolaTheCoder <isacmatolajr6@gmail.com>
2025-01-04 01:21:28 +0200

    feat: refactor styling and importing inputGroup

--- src/components/ProdutosAdd.jsx
+++ src/components/ProdutosAdd.jsx
@@ -1,49 +1,33 @@
 import React from 'react';
+import InputGroup from './reusable/Input';
 
 export default function ProdutosAdd() {
   return (
-    <div className="container w-full p-4 bg-white flex justify-center items-center ">
-      <form action="" method="post" className="w-full max-w-3xl">
-        <div>
-          <div className="w-full flex justify-center text-gray-700 font-bold pb-5">
-            <span>Adicionar produtos</span>
-          </div>
-          <div className="w-full m-auto">
-            <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
-              <InputGroup label1="Nome do produto" label2="Categoria" label3="Data de criação" />
-              <InputGroup label1="Preço do produto" label2="Produto Backlog" label3="Produto Terciários" />
+    <div className=' flex flex-col min-h-screen w-full'>
+      <div className="title text-gray-700 flex text-lg p-4 font-semibold ">
+        <p className=''>Produtos</p>
+      </div>
+      <div className="container w-full p-4 bg-white flex justify-center items-center ">
+        <form action="" method="post" className="w-full max-w-3xl p-6 shadow-lg">
+          <div>
+            <div className="w-full flex justify-center text-gray-700 font-bold pb-5">
+              <span>Adicionar produtos</span>
+            </div>
+            <div className="w-full m-auto">
+              <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
+                <InputGroup label1="Nome do produto" label2="Categoria" label3="Data de criação" />
+                <InputGroup label1="Preço do produto" label2="Produto Backlog" label3="Produto Terciários" />
+              </div>
             </div>
           </div>
-        </div>
-        <div className="flex justify-center mt-6">
-          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-            Adicionar Produto
-          </button>
-        </div>
-      </form>
+          <div className="flex justify-center p-8">
+            <button className="w-4/5 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+              Adicionar Produto
+            </button>
+          </div>
+        </form>
+      </div>
     </div>
   );
 }
 
-function InputGroup({ label1, label2, label3 }) {
-  return (
-    <div className="flex flex-col space-y-4">
-      <InputWrapper label={label1} />
-      {label2 && <InputWrapper label={label2} />}
-      {label3 && <InputWrapper label={label3} />}
-    </div>
-  );
-}
-
-function InputWrapper({ label }) {
-  return (
-    <div className="w-full">
-      <label className="block text-gray-700 font-medium mb-1">{label}</label>
-      <input
-        type="text"
-        placeholder={label}
-        className="w-full px-4 py-2 border border-gray-400 outline-gray-300 text-gray-600 rounded"
-      />
-    </div>
-  );
-}
