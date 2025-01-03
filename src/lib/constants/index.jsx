07d413bc0604f4a6a90cc3a4033adb3847357fee
MatolaTheCoder <isacmatolajr6@gmail.com>
2025-01-04 01:24:21 +0200

    feat: refactor the links

--- src/lib/constants/index.jsx
+++ src/lib/constants/index.jsx
@@ -5,54 +5,62 @@ import {
     HiOutlineUsers,
     HiOutlineDocumentText,
     HiOutlineQuestionMarkCircle,
-    HiOutlineCog
-} from 'react-icons/hi'
+    HiOutlineCog,
+    HiOutlineChevronDown,
+} from 'react-icons/hi';
 
 export const DASHBOARD_SIDEBAR_LINKS = [
     {
         key: 'dashboard',
         label: 'Dashboard',
         path: '/',
-        icon: <HiOutlineViewGrid/>
+        icon: <HiOutlineViewGrid />,
     },
     {
-        key: 'products',
+        key: 'productos',
         label: 'Produtos',
         path: '/produtos',
-        icon: <HiOutlineCube/>
+        icon: <HiOutlineCube />,
+        children: [
+            { key: 'produtosAdd', label: 'Adicionar', path: '/produtos/add' },
+            { key: 'produtosList', label: 'Listar', path: '/produtos/list' },
+        ],
     },
     {
-        key: 'orders',
+        key: 'encomendas',
         label: 'Encomendas',
-        path: '/orders',
-        icon: <HiOutlineShoppingCart/>
+        path: '/encomendas',
+        icon: <HiOutlineShoppingCart />,
+        children: [
+            { key: 'EncomendasAdd', label: 'Adicionar', path: '/encomendas/add' },
+            { key: 'EncomendasList', label: 'Listar', path: '/encomendas/list' },
+        ],
     },
     {
         key: 'customers',
         label: 'Clientes',
         path: '/customers',
-        icon: <HiOutlineUsers/>
+        icon: <HiOutlineUsers />,
     },
     {
         key: 'transactions',
-        label: 'Transaccoes',
-        path: '/transactions',
-        icon: <HiOutlineDocumentText/>        
-    }
-]
+        label: 'Transacções',
+        path: '/pagamento',
+        icon: <HiOutlineDocumentText />,
+    },
+];
 
-export const DASHBOARD_SIDEBAR_BOTTOM_LINKS=[
+export const DASHBOARD_SIDEBAR_BOTTOM_LINKS = [
     {
         key: 'settings',
-        label: 'Definicoes',
+        label: 'Definições',
         path: '/settings',
-        icon: <HiOutlineCog/>
+        icon: <HiOutlineCog />,
     },
     {
         key: 'support',
         label: 'Ajuda & Apoio',
         path: '/support',
-        icon: <HiOutlineQuestionMarkCircle/>
-    }
-]
-
+        icon: <HiOutlineQuestionMarkCircle />,
+    },
+];
