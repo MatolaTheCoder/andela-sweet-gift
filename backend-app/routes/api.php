0ea7a6af961c77cbb8cf14b9983ba0cec3326b7a
MatolaTheCoder <isacmatolajr6@gmail.com>
2025-01-06 00:47:05 +0200

    feat: authentication features and token support

--- backend-app/routes/api.php
+++ backend-app/routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\EncomendaController;
 use App\Http\Controllers\ProdutoController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -8,4 +10,10 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::apiResource('products', ProdutoController::class);
+
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
+
+Route::apiResource('products', ProdutoController::class)->middleware('auth:sanctum');
+Route::apiResource('encomenda', EncomendaController::class)->middleware('auth:sanctum');
