9e4bf8e96daca889d99a12fd023e68b40cd5d290
MatolaTheCoder <isacmatolajr6@gmail.com>
2025-01-05 21:55:19 +0200

    feat: products route

--- backend-app/routes/api.php
+++ backend-app/routes/api.php
@@ -1,8 +1,11 @@
 <?php
 
+use App\Http\Controllers\ProdutoController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::apiResource('products', ProdutoController::class);
