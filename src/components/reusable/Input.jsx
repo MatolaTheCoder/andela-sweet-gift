6dfeb63aca2f70595400c4dae45bc8f99bfdfe16
MatolaTheCoder <isacmatolajr6@gmail.com>
2025-01-06 14:04:51 +0200

    chore: removed the comments

--- src/components/reusable/Input.jsx
+++ src/components/reusable/Input.jsx
@@ -1,15 +1,5 @@
 import React from 'react'
 
-// function InputGroup({ label1, label2, label3 }) {
-//   return (
-//     <div className="flex flex-col space-y-6">
-//       <InputWrapper label={label1} />
-//       {label2 && <InputWrapper label={label2} />}
-//       {label3 && <InputWrapper label={label3} />}
-//     </div>
-//   );
-// }
-
 function InputWrapper({ label, name, value, onChange, disabled, type, options }) {
   return (
     <div className="w-full mb-3">
