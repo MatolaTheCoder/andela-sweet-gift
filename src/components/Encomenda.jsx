26a8bd293c76a4177816769ed989c627858f40d1
MatolaTheCoder <isacmatolajr6@gmail.com>
2025-01-06 14:22:12 +0200

    feat: handling the orders fetch

--- src/components/Encomenda.jsx
+++ src/components/Encomenda.jsx
@@ -1,66 +1,30 @@
-import { Dialog } from '@headlessui/react';
-import React, { useState } from 'react'
-import { MdDelete, MdModeEdit } from 'react-icons/md';
-import { Link } from 'react-router-dom';
+import { Dialog } from '@headlessui/react' 
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
+import { MdDelete, MdModeEdit } from 'react-icons/md' 
+import { Link } from 'react-router-dom' 
 
 export default function Encomenda() {
-    const recentData = [
-        {
-            id: '1',
-            productId: 'PROD001',
-            customerName: 'Alice Smith',
-            orderDate: '2024-04-21:09:30',
-            orderTotal: '850MZN',
-            orderStatus: 'pending',
-            address: '123 Main Street'
-        },
-        {
-            id: '2',
-            productId: 'PROD002',
-            customerName: 'Bob Johnson',
-            orderDate: '2024-04-20:15:45',
-            orderTotal: '1200MZN',
-            orderStatus: 'pending',
-            address: '456 Elm Avenue'
-        },
-        {
-            id: '3',
-            productId: 'PROD003',
-            customerName: 'Eve Brown',
-            orderDate: '2024-04-19:12:00',
-            orderTotal: '2100MZN',
-            orderStatus: 'delivered',
-            address: '789 Oak Road'
-        },
-        {
-            id: '4',
-            productId: 'PROD004',
-            customerName: 'Charlie Davis',
-            orderDate: '2024-04-18:18:20',
-            orderTotal: '1750MZN',
-            orderStatus: 'Ccncelled',
-            address: '1011 Pine Lane'
-        },
-        {
-            id: '5',
-            productId: 'PROD005',
-            customerName: 'Charlie Davis',
-            orderDate: '2024-04-18:18:20',
-            orderTotal: '1750MZN',
-            orderStatus: 'delivered',
-            address: '1011 Pine Lane'
-        },
-        {
-            id: '6',
-            productId: 'PROD006',
-            customerName: 'Charlie Davis',
-            orderDate: '2024-04-18:18:20',
-            orderTotal: '1750MZN',
-            orderStatus: 'cancelled',
-            address: '1011 Pine Lane'
+    const [encomendas, setEncomendas] = useState([])     
+    const [isOpen, setIsOpen] = useState(false) 
+    useEffect(() => {
+        axios.defaults.baseURL = 'http://localhost:8000'
+        axios.defaults.headers.common['Authorization'] = `Bearer ${localStorage.getItem('token')}`
+    }, [])
+
+    useEffect(()=>{
+        const fetchEncomendas = async () => {
+            try {
+                const response = await axios.get('/api/orders')
+                setEncomendas(response.data)
+                console.log(response.data)
+            } catch (error) {
+                console.log('Erro: ',error)
+            }
         }
-    ];
-    const [isOpen, setIsOpen] = useState(false);
+        fetchEncomendas()
+    },[])
+
     return (
         <div>
             <div className=' flex flex-col min-h-screen w-full'>
@@ -77,14 +41,16 @@ export default function Encomenda() {
                                         <th className="text-[12px] uppercase tracking-wide font-medium py-3 px-4 text-left">id</th>
                                         <th className="text-[12px] uppercase tracking-wide font-medium py-3 px-4 text-left">Nome do produto</th>
                                         <th className="text-[12px] uppercase tracking-wide font-medium py-3 px-4 text-left">Nome do cliente</th>
-                                        <th className="text-[12px] uppercase tracking-wide font-medium py-3 px-4 text-left">Data de reserva</th>
+                                        <th className="text-[12px] uppercase tracking-wide font-medium py-3 px-4 text-left">Nome do destinatario</th>
+                                        <th className="text-[12px] uppercase tracking-wide font-medium py-3 px-4 text-left">Data de criacao</th>
                                         <th className="text-[12px] uppercase tracking-wide font-medium py-3 px-4 text-left">Data de entrega</th>
+                                        <th className="text-[12px] uppercase tracking-wide font-medium py-3 px-4 text-left">Quantidade</th>
                                         <th className="text-[12px] uppercase tracking-wide font-medium py-3 px-4 text-left">Valor</th>
-                                        <th className="text-[12px] uppercase tracking-wide font-medium py-3 px-4 text-left">Estado</th>
+                                        <th className="text-[12px] uppercase tracking-wide font-medium py-3 px-4 text-left">Opcoes</th>
                                     </tr>
                                 </thead>
                                 <tbody className="text-gray-700">
-                                    {recentData.map((data, index) => (
+                                    {encomendas.map((data, index) => (
                                         <tr
                                             key={data.id}
                                             className={`${index % 2 === 0 ? "bg-gray-50" : "bg-white"
@@ -100,30 +66,40 @@ export default function Encomenda() {
                                             </td>
                                             <td className="py-3 px-4 border-b">
                                                 <span className="text-[13px] font-medium text-gray-500">
-                                                    {data.customerName}
+                                                    {data.id_produto}
                                                 </span>
                                             </td>
                                             <td className="py-3 px-4 border-b">
                                                 <span className="text-[13px] font-medium text-gray-500">
-                                                    {data.productId}
+                                                    {data.solicitante}
                                                 </span>
                                             </td>
                                             <td className="py-3 px-4 border-b">
                                                 <span className="text-[13px] font-medium text-gray-500">
-                                                    {data.orderDate}
+                                                    {data.destinatario}
                                                 </span>
                                             </td>
                                             <td className="py-3 px-4 border-b">
                                                 <span className="text-[13px] font-medium text-gray-500">
-                                                    {data.orderDate}
+                                                    {data.data_criacao}
                                                 </span>
                                             </td>
                                             <td className="py-3 px-4 border-b">
                                                 <span className="text-[13px] font-medium text-gray-500">
-                                                    {data.orderStatus}
+                                                    {data.data_entrega}
                                                 </span>
                                             </td>
-                                            <td className="py-3 px-4 border-b flex items-center gap-2">
+                                            <td className="py-3 px-4 border-b">
+                                                <span className="text-[13px] font-medium text-gray-500">
+                                                    {data.quantidade}
+                                                </span>
+                                            </td>
+                                            <td className="py-3 px-4 border-b">
+                                                <span className="text-[13px] font-medium text-gray-500">
+                                                    {data.valor}
+                                                </span>
+                                            </td>
+                                            <td className="py-3 px-4 border-b flex justify-center items-center gap-2">
                                                 <Link
                                                     to="#"
                                                     className="text-sky-500 hover:text-sky-700 transition-transform transform hover:scale-110"
